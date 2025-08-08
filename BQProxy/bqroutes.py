ec4c36eeb9e2f4bc87543bf73235c303513f8669
S. Paquette <spaquett@systemsbiology.org>
2025-08-08 11:54:32 -0700

    -> Deployment of the BQ Proxy

--- BQProxy/bqroutes.py
+++ BQProxy/bqroutes.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from flask import jsonify
 from google.auth.transport.requests import Request
 import json
@@ -7,28 +8,33 @@ from collab_queries import set_queries
 from google.oauth2 import service_account
 from google.auth import default
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-# set the env variable "credentials_file_needed" to true and env variable 'credentials_path'' to the file path containing the gcp credentials file
-# if running locally
-
-
-project_id = os.environ["PROJECT_ID"]
-credentials_file_needed = ((os.environ["CREDENTIALS_FILE_NEEDED"]).lower() == "true")
-if credentials_file_needed:
-    credentials_file=os.environ['CREDENTIALS_FILE']
-    credentials = service_account.Credentials.from_service_account_file(credentials_file, scopes=[
-    'https://www.googleapis.com/auth/cloud-platform'])
-else:
-    credentials, _ = default()
+# Project ID from app.yaml
+project_id = os.environ.get("PROJECT_ID")
 
 # Endpoints and URLS for proxy and GCS BQ
 job_url = f"https://bigquery.googleapis.com/bigquery/v2/projects/{project_id}/jobs"
-pjob_path=f"/bqproxy/bigquery/v2/projects/{project_id}/jobs"
-pjob_path_parts=len(pjob_path.split('/'))
+pjob_path = f"/bqproxy/bigquery/v2/projects/{project_id}/jobs"
 
-query_url=f"https://bigquery.googleapis.com/bigquery/v2/projects/{project_id}/queries"
-pquery_path=f"/bqproxy/bigquery/v2/projects/{project_id}/queries"
-pquery_path_parts=len(pjob_path.split('/'))
+query_url = f"https://bigquery.googleapis.com/bigquery/v2/projects/{project_id}/queries"
+pquery_path = f"/bqproxy/bigquery/v2/projects/{project_id}/queries"
+
+# set the env variable "credentials_file_needed" to true and env variable 'credentials_path'' to the file path
+# containing the gcp credentials file if running locally
+credentials_file_needed = ((os.environ.get("CREDENTIALS_FILE_NEEDED", "false")).lower() == "true")
+if credentials_file_needed:
+    credentials_file = os.environ.get('CREDENTIALS_FILE', None)
+    if credentials_file:
+        credentials = service_account.Credentials.from_service_account_file(credentials_file, scopes=[
+            'https://www.googleapis.com/auth/cloud-platform'])
+    else:
+        raise Exception("Credential file indicated but not found. " +
+                        "Path and file name provided: {}".format(credentials_file)
+                        )
+else:
+    credentials, _ = default()
 
 # Job configuration template
 job_config = {
@@ -41,65 +47,64 @@ job_config = {
 }
 
 
+def api_interface(request, jobId):
+    resp_data = { "error": "unsuccessful" }
+    try:
+        credentials.refresh(Request())
+        access_token = credentials.token
+        url = query_url+'/'+jobId
+        headers = {
+            "Authorization": f"Bearer {access_token}",
+            "Content-Type": "application/json"
+        }
+        response = requests.get(url, headers=headers)
+        resp_data = json.loads(response.content)
+    except Exception as e:
+        logger.error("[ERROR] While calling API endpoint for job {}:".format(jobId))
+        logger.exception(e)
+    return jsonify(resp_data)
 
 
 def submit_job(request):
-    resp_data={"error":"unsuccessful"}
-    credentials.refresh(Request())
-    access_token = credentials.token
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-        "Content-Type": "application/json"
-    }
-    data = request.get_json()
-    if ('jobReference' in data):
-        data['jobReference']['projectId']=project_id
-    # Send the POST request
-    if ('configuration' in data) and ('query' in data['configuration']) and ('queryParameters' in data['configuration']['query']):
-        #erase whatever initial query is
-        data['configuration']['query']['query'] = ''
-        nparams = []
-        for param in data['configuration']['query']['queryParameters']:
-            # look for a queryid in the params and use that to set the sql
-            if (param['name'] == "queryid") and (param['parameterValue']['value'] in set_queries):
-                queryid=param['parameterValue']['value']
-                data['configuration']['query']['query'] = set_queries[queryid]["sql"]
+    resp_data = { "error": "unsuccessful" }
+    try:
+        credentials.refresh(Request())
+        access_token = credentials.token
+        headers = {
+            "Authorization": f"Bearer {access_token}",
+            "Content-Type": "application/json"
+        }
+        data = request.get_json()
+        if 'jobReference' in data:
+            data['jobReference']['projectId'] = project_id
+        # Send the POST request
+        if ('configuration' in data) and ('query' in data['configuration']) and ('queryParameters' in data['configuration']['query']):
+            # erase whatever initial query is
+            data['configuration']['query']['query'] = ''
+            nparams = []
+            for param in data['configuration']['query']['queryParameters']:
+                # look for a queryid in the params and use that to set the sql
+                if (param['name'] == "queryid") and (param['parameterValue']['value'] in set_queries):
+                    queryid = param['parameterValue']['value']
+                    data['configuration']['query']['query'] = set_queries[queryid]["sql"]
+                else:
+                    nparams.append(param)
+            if len(nparams) > 0:
+                data['configuration']['query']['queryParameters'] = nparams
             else:
-                nparams.append(param)
-        if len(nparams) > 0:
-            data['configuration']['query']['queryParameters'] = nparams
-        else:
-            del data['configuration']['query']['queryParameters']
-        jdata = json.dumps(data)
-        response = requests.post(job_url, headers=headers, data=jdata)
-        resp_data=json.loads(response.content)
-
+                del data['configuration']['query']['queryParameters']
+            jdata = json.dumps(data)
+            response = requests.post(job_url, headers=headers, data=jdata)
+            resp_data = json.loads(response.content)
+    except Exception as e:
+        logger.error("[ERROR] In job submission: ")
+        logger.exception(e)
     return jsonify(resp_data)
 
 
-def check_job(request,jobid):
-    resp_data = {"error": "unsuccessful"}
-    credentials.refresh(Request())
-    access_token = credentials.token
-    url=job_url+'/'+jobid
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-        "Content-Type": "application/json"
-    }
-    response = requests.get(url, headers=headers)
-    resp_data = json.loads(response.content)
-    return jsonify(resp_data)
+def check_job(request, jobId):
+    return api_interface(request, jobId)
 
 
-def get_results(request,jobid):
-    resp_data = {"error": "unsuccessful"}
-    credentials.refresh(Request())
-    access_token = credentials.token
-    url=query_url+'/'+jobid
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-        "Content-Type": "application/json"
-    }
-    response = requests.get(url, headers=headers)
-    resp_data = json.loads(response.content)
-    return jsonify(resp_data)
+def get_results(request, jobId):
+    return api_interface(request, jobId)
--- BQProxy/main.py
+++ BQProxy/main.py
@@ -1,9 +1,32 @@
-from flask import Flask, request
-from bqroutes import pjob_path,  pquery_path
+import logging
+import os
+from flask import Flask, request, jsonify
+from flask_talisman import Talisman
+from bqroutes import pjob_path, pquery_path
 from bqroutes import submit_job, check_job, get_results
 
+IS_APP_ENGINE = os.environ.get("IS_APP_ENGINE", "false").lower() == 'true'
+
+if IS_APP_ENGINE:
+    import google.cloud.logging
+    client = google.cloud.logging_v2.Client()
+    client.setup_logging()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 app = Flask(__name__)
+if IS_APP_ENGINE:
+    Talisman(app, strict_transport_security_max_age=300, content_security_policy={
+        'default-src': [
+            '\'self\'',
+            '*.googleapis.com',
+            '*.swagger.io',
+            '\'unsafe-inline\'',
+            'data:'
+        ]
+    })
+
 
 @app.route(pjob_path, methods=['POST'])
 def flask_submit_job():
@@ -12,11 +35,26 @@ def flask_submit_job():
 
 @app.route(pjob_path+'/'+'<path:jobid>', methods=['GET'])
 def flask_check_job(jobid):
-    return check_job(request,jobid)
+    return check_job(request, jobid)
+
 
 @app.route(pquery_path+'/'+'<string:jobid>', methods=['GET'])
 def flask_get_results(jobid):
-    return get_results(request,jobid)
+    return get_results(request, jobid)
+
+
+# Error handlers
+@app.errorhandler(500)
+def unexpected_error(e):
+    """Handle exceptions by returning swagger-compliant json."""
+    logger.error('[ERROR] An error occurred while processing the request:')
+    logger.exception(e)
+    response = jsonify({
+        'code': 500,
+        'message': 'Exception: {}'.format(e)
+    })
+    response.status_code = 500
+    return response
 
 
 if __name__ == '__main__':
