2bbab35af5ba9ade249955424b8dae0b44967ec4
George <george@butter.local>
2025-08-18 09:52:45 -0700

    add a redis cache to track and limit daily usage by ip; add an option for users to send in their own queries which is controlled by the USER_QUERY_OK environmental variable

--- BQProxy/collab_queries.py
+++ BQProxy/collab_queries.py
@@ -1,3 +1,52 @@
+query_fragments={}
+
+query_fragments["difexp_caseq"]= """WITH rna as (
+    SELECT
+       case_barcode,
+       sample_barcode,
+       aliquot_barcode,
+       Ensembl_gene_id_v,
+       gene_name,
+       unstranded,
+       fpkm_uq_unstranded,
+       sample_type_name,
+    FROM `isb-cgc-bq.TCGA_versioned.RNAseq_hg38_gdc_r35`
+    WHERE gene_type = 'protein_coding'
+    AND project_short_name = '@proj_nm'
+),
+cases as (
+  SELECT case_barcode, agg FROM
+      (SELECT
+        case_barcode,
+        array_agg(distinct sample_type_name) agg
+      FROM rna
+      GROUP BY case_barcode)
+  WHERE array_length(agg) > 1
+  AND ("Solid Tissue Normal" in UNNEST(agg))
+  )"""
+
+query_fragments["difexp_scase"]="""SELECT * FROM cases"""
+
+query_fragments["difexp_expq"]= """,
+mean_expr as (
+  SELECT * FROM (
+    SELECT
+      rna.Ensembl_gene_id_v,
+      VARIANCE(rna.fpkm_uq_unstranded) var_fpkm
+    FROM rna
+    JOIN cases ON rna.case_barcode = cases.case_barcode
+    WHERE rna.sample_type_name = 'Solid Tissue Normal'
+    GROUP BY rna.Ensembl_gene_id_v)
+  ORDER BY var_fpkm DESC
+  LIMIT 2000)"""
+
+query_fragments["difexp_selgenes"]="""
+SELECT * FROM mean_expr
+"""
+
+
+
+
 set_queries={}
 
 
